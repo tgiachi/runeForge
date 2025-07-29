b615a0f67ef574251b01e57fd2149067fbd6b852
tom <squid@stormwind.it>
2025-07-29 16:17:29 +0200

    feat(ScriptedAction): add new ScriptedAction class to enable custom actions
    Introduce ScriptedAction class to allow users to define custom actions
    by passing an Action delegate. This enhances flexibility and
    extensibility of the action execution system.

---
+++ src/Runeforge.Engine/Ticks/Actions/ScriptedAction.cs
@@ -0,0 +1,21 @@
+using Runeforge.Engine.Types.Tick;
+
+namespace Runeforge.Engine.Ticks.Actions;
+
+public class ScriptedAction : BaseAction
+{
+    private Action _action;
+    
+    public ScriptedAction(Action action)
+    {
+        _action = action;
+    }
+
+    public override ActionPriority Priority { get; } = ActionPriority.Normal;
+    public override int Speed { get; set; } = 1;
+    protected override ActionResult ExecuteAction()
+    {
+        return ActionResult.Success;
+
+    }
+}
